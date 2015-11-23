1a7b91afc5c9092ded04881cc3c7c076251b1602
Michael Hearne <mhearne@usgs.gov>
2015-11-23 08:33:28 -0700

    Changed copyright in docs to usgs

--- rest/source/conf.py
+++ rest/source/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MapIO'
-copyright = u'2015, Mike Hearne'
+copyright = u'2015, United States Geological Survey'
 author = u'Mike Hearne'
 
 # The version info for the project you're documenting, acts as replacement for
