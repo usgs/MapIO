8e27343b1b536979417c2062bb4ffe5a766ea28d
Thompson <emthompson@usgs.gov>
2017-11-28 10:58:18 -0700

    Fixed package setup for importing

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='mapio',
       author='Mike Hearne,Bruce Worden',
       author_email='mhearne@usgs.gov,cbworden@usgs.gov',
       url='',
-      packages=['mapio'],
+      packages=['mapio', 'mapio.extern'],
       scripts = [],
 )
